454b65404ab651ad526439f329b868476aa13415
Pavel Busovikov <pavel.busovikov@gmail.com>
2021-07-05 23:46:14 +0300

    Add minimal initialization for App

---
+++ Hlms.h
@@ -0,0 +1,82 @@
+﻿#pragma once
+
+#include "OgreConfigFile.h"
+#include <OgreArchiveManager.h>
+#include "OgreHlmsManager.h"
+#include <OgreHlmsPbs.h>
+#include "OgreHlmsUnlit.h"
+
+template<class T>
+T* CreateHlms(const Ogre::String& rootFolder);
+
+void registerHlms( void )
+{
+    using namespace Ogre;
+
+    String resourcePath = "data/";
+
+    ConfigFile cf;
+    cf.load( resourcePath + "Resources.cfg" );
+    String rootHlmsFolder = resourcePath + cf.getSetting( "DoNotUseAsResource", "Hlms", "" );
+
+    if( rootHlmsFolder.empty() )
+        rootHlmsFolder = "./";
+    else if( *( rootHlmsFolder.end() - 1 ) != '/' )
+        rootHlmsFolder += "/";
+
+    // At this point rootHlmsFolder should be a valid path to the Hlms data folder
+
+    Root::getSingleton().getHlmsManager()->registerHlms( CreateHlms<HlmsUnlit>(rootHlmsFolder) );
+    Root::getSingleton().getHlmsManager()->registerHlms( CreateHlms<HlmsPbs>(rootHlmsFolder) );
+}
+
+template<class T>
+T* CreateHlms(const Ogre::String& rootFolder)
+{
+    using namespace Ogre;
+
+    // For retrieval of the paths to the different folders needed
+    String mainFolderPath;
+    StringVector libraryFoldersPaths;
+    StringVector::const_iterator libraryFolderPathIt;
+    StringVector::const_iterator libraryFolderPathEn;
+
+    ArchiveManager &archiveManager = ArchiveManager::getSingleton();
+
+    // Create & Register T (Hlms implementation)
+    // Get the path to all the subdirectories used by HlmsUnlit
+    T::getDefaultPaths( mainFolderPath, libraryFoldersPaths );
+    ArchiveVec archiveLibraryFolders;
+    Archive *archive =
+        archiveManager.load( rootFolder + mainFolderPath, "FileSystem", true );
+    libraryFolderPathIt = libraryFoldersPaths.begin();
+    libraryFolderPathEn = libraryFoldersPaths.end();
+    while( libraryFolderPathIt != libraryFolderPathEn )
+    {
+        Archive *archiveLibrary =
+            archiveManager.load( rootFolder + *libraryFolderPathIt, "FileSystem", true );
+        archiveLibraryFolders.push_back( archiveLibrary );
+        ++libraryFolderPathIt;
+    }
+
+    // Create and register the Hlms
+    T* hlms = OGRE_NEW T( archive, &archiveLibraryFolders );
+
+    RenderSystem *renderSystem = Root::getSingletonPtr()->getRenderSystem();
+    if( renderSystem->getName() == "Direct3D11 Rendering Subsystem" )
+    {
+        // Set lower limits 512kb instead of the default 4MB per Hlms in D3D 11.0
+        // and below to avoid saturating AMD's discard limit (8MB) or
+        // saturate the PCIE bus in some low end machines.
+        bool supportsNoOverwriteOnTextureBuffers;
+        renderSystem->getCustomAttribute( "MapNoOverwriteOnDynamicBufferSRV",
+                                        &supportsNoOverwriteOnTextureBuffers );
+
+        if( !supportsNoOverwriteOnTextureBuffers )
+        {
+            hlms->setTextureBufferDefaultSize( 512 * 1024 );
+        }
+    }
+    return hlms;
+}
+        
---
+++ main.cpp
@@ -0,0 +1,91 @@
+﻿#include "Hlms.h"
+
+#include "OgreCamera.h"
+#include "OgreRoot.h"
+#include "OgreWindow.h"
+#include "Compositor/OgreCompositorManager2.h"
+#include "OgreWindowEventUtilities.h"
+
+class MyWindowEventListener : public Ogre::WindowEventListener
+{
+    bool mQuit;
+
+public:
+    MyWindowEventListener() : mQuit( false ) {}
+    virtual void windowClosed( Ogre::Window *rw ) { mQuit = true; }
+
+    bool getQuit( void ) const { return mQuit; }
+};
+
+int main( int argc, const char *argv[] )
+{
+    using namespace Ogre;
+
+    const String pluginsFolder = "./data/";
+    const String writeAccessFolder = "./";
+
+#ifndef OGRE_STATIC_LIB
+#    if OGRE_DEBUG_MODE
+    const char *pluginsFile = "plugins_d.cfg";
+#    else
+    const char *pluginsFile = "plugins.cfg";
+#    endif
+#endif
+    Root *root = OGRE_NEW Root( pluginsFolder + pluginsFile,    
+                                writeAccessFolder + "ogre.cfg",  
+                                writeAccessFolder + "Ogre.log" );
+
+    Ogre::RenderSystem* renderSystem = root->getRenderSystemByName("Direct3D11 Rendering Subsystem");
+	if (!(renderSystem))
+	{
+		printf("Render system not found!\n");
+		return -1;
+	}
+    // Initialize Root
+    root->setRenderSystem(renderSystem);
+    root->getRenderSystem()->setConfigOption( "sRGB Gamma Conversion", "Yes" );
+    root->getRenderSystem()->setConfigOption( "Full Screen", "No" );
+    Window *window = root->initialise( true, "Tutorial 00: Basic" );
+
+    //registerHlms();
+
+    // Create SceneManager
+    const size_t numThreads = 1u;
+    SceneManager *sceneManager = root->createSceneManager( ST_GENERIC, numThreads, "ExampleSMInstance" );
+
+    // Create & setup camera
+    Camera *camera = sceneManager->createCamera( "Main Camera" );
+
+    // Position it at 500 in Z direction
+    camera->setPosition( Vector3( 0, 5, 15 ) );
+    // Look back along -Z
+    camera->lookAt( Vector3( 0, 0, 0 ) );
+    camera->setNearClipDistance( 0.2f );
+    camera->setFarClipDistance( 1000.0f );
+    camera->setAutoAspectRatio( true );
+
+    // Setup a basic compositor with a blue clear colour
+    CompositorManager2 *compositorManager = root->getCompositorManager2();
+    const String workspaceName( "Demo Workspace" );
+    const ColourValue backgroundColour( 0.2f, 0.4f, 0.6f );
+    compositorManager->createBasicWorkspaceDef( workspaceName, backgroundColour, IdString() );
+    compositorManager->addWorkspace( sceneManager, window->getTexture(), camera, workspaceName, true );
+
+    MyWindowEventListener myWindowEventListener;
+    WindowEventUtilities::addWindowEventListener( window, &myWindowEventListener );
+
+    bool bQuit = false;
+
+    while( !bQuit )
+    {
+        WindowEventUtilities::messagePump();
+        bQuit = myWindowEventListener.getQuit() || !root->renderOneFrame();
+    }
+
+    WindowEventUtilities::removeWindowEventListener( window, &myWindowEventListener );
+
+    OGRE_DELETE root;
+    root = 0;
+
+    return 0;
+}
